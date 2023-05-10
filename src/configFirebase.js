36362b54c8d4e346fcbd5c6b5b545843a20fb56c
Bryan <bryansanudo95@gmail.com>
2023-05-10 09:13:16 -0500

    add config firebase

---
+++ src/configFirebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqV4KA4xkd7yWLw5ggKOiQxJlRH06XGTE",
+  authDomain: "dealer-app-a4d2a.firebaseapp.com",
+  projectId: "dealer-app-a4d2a",
+  storageBucket: "dealer-app-a4d2a.appspot.com",
+  messagingSenderId: "963100383143",
+  appId: "1:963100383143:web:7ad96640643eca75804882",
+};
+
+const appFirebase = initializeApp(firebaseConfig);
+export const auth = getAuth(appFirebase);
